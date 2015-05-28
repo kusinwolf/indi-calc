486c27cbad6c0b31eddb3dccb494386ac578be2b
kusinwolf <kusinwolf@gmail.com>
2015-05-27 21:54:07 -0500

    Base structures & materials for PI
    All of the Planetary interaction materials and their mappings for
    construction. Also included first pass structure to class hierarchy.

---
+++ base.py
@@ -0,0 +1,138 @@
+""" I feel as though I'm over doing this o_o """
+from copy import deepcopy
+
+
+class MarketItem(object):
+    def __init__(self, jita_price=0.0, zero_zero_price=0.0, name=None):
+        """
+        :param jita_price: Price at said location
+        :type jita_price: float
+        :param zero_zero_price: Price at said location
+        :type zero_zero_price: float
+        """
+        self.jita_price = jita_price
+        self.zero_zero_price = zero_zero_price
+        self.name = name
+
+    def __repr__(self):
+        return "<{}: '{}'>".format(
+            self.__class__.__name__, self.name
+        )
+
+    def __hash__(self):
+        return self.name.lower().__hash__()
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+class Manufacturable(MarketItem):
+    def __init__(
+        self, requirements={}, produces=0, processing_time=0, *args, **kwargs
+    ):
+        """
+        Basically a blueprint original or copy
+
+        :param requirements: What items are required to produce this
+            {Item: Amount}
+        :type requirements: dict
+        :param produces: How many items are produced from one run
+        :type produces: int
+        :param processing_time: Time in seconds for how long it takes
+        :type processing_time: int
+        """
+        self.material_efficiency = 10
+        self.time_efficiency = 20
+        self.requirements = requirements
+        self.processing_time = processing_time  # Time in seconds
+        self.produces = produces
+        super(Manufacturable, self).__init__(*args, **kwargs)
+
+    def __mul__(self, multiplier):
+        new_object = deepcopy(self)
+
+        new_object.processing_time = new_object.processing_time * multiplier
+
+        for key, value in new_object.requirements.items():
+            new_object.requirements[key] = (value * multiplier)
+
+        return new_object
+
+
+class PlanetaryInteractionMaterial(Manufacturable):
+    def __init__(
+        self, base_import_export_fee=0.0, tax_rate=1.02, *args, **kwargs
+    ):
+        """
+        Basically a blueprint original or copy
+
+        :param base_import_export_fee: How much is the base cost to
+            import/export the materials
+        :type base_import_export_fee: float
+        :param tax_rate: What is the tax rate set by the owner
+        :type tax_rate: float
+        """
+        self.base_import_export_fee = base_import_export_fee
+        self.tax_rate = tax_rate
+        super(PlanetaryInteractionMaterial, self).__init__(*args, **kwargs)
+
+    def get_export_fee(self):
+        return self.base_import_export_fee * self.tax_rate * 1.5
+
+    def get_import_fee(self):
+        return self.base_import_export_fee * self.tax_rate * 0.5
+
+
+class RawMaterial(PlanetaryInteractionMaterial):
+    def __init__(self, *args, **kwargs):
+        super(RawMaterial, self).__init__(
+            base_import_export_fee=4.0,
+            processing_time=0,
+            produces=1,
+            *args,
+            **kwargs
+        )
+
+
+class ProcessedMaterials(PlanetaryInteractionMaterial):
+    def __init__(self, *args, **kwargs):
+        super(ProcessedMaterials, self).__init__(
+            base_import_export_fee=400.0,
+            processing_time=30 * 60,
+            produces=20,
+            *args,
+            **kwargs
+        )
+
+
+class RefinedCommodities(PlanetaryInteractionMaterial):
+    def __init__(self, *args, **kwargs):
+        super(RefinedCommodities, self).__init__(
+            base_import_export_fee=7200.0,
+            processing_time=60 * 60,
+            produces=5,
+            *args,
+            **kwargs
+        )
+
+
+class SpecializedCommodities(PlanetaryInteractionMaterial):
+    def __init__(self, *args, **kwargs):
+        super(SpecializedCommodities, self).__init__(
+            base_import_export_fee=60000.0,
+            processing_time=60 * 60,
+            produces=3,
+            *args,
+            **kwargs
+        )
+
+
+class AdvancedCommodities(PlanetaryInteractionMaterial):
+    def __init__(self, *args, **kwargs):
+        super(AdvancedCommodities, self).__init__(
+            base_import_export_fee=1200000.0,
+            processing_time=60 * 60,
+            produces=1,
+            *args,
+            **kwargs
+        )
---
+++ materials.py
@@ -0,0 +1,319 @@
+
+from base import AdvancedCommodities
+from base import Manufacturable
+from base import MarketItem
+from base import ProcessedMaterials
+from base import RawMaterial
+from base import RefinedCommodities
+from base import SpecializedCommodities
+
+
+# (T0) Raw Materials
+AqueousLiquids = RawMaterial(name="Aqueous Liquids")
+BaseMetals = RawMaterial(name="Base Metals")
+ComplexOrganisms = RawMaterial(name="Complex Organisms")
+HeavyMetals = RawMaterial(name="Heavy Metals")
+MicroOrganisms = RawMaterial(name="Micro Organisms")
+NobleMetals = RawMaterial(name="Noble Metals")
+PlankticColonies = RawMaterial(name="Planktic Colonies")
+SuspendedPlasma = RawMaterial(name="Suspended Plasma")
+Autotrophs = RawMaterial(name="Autotrophs")
+CarbonCompounds = RawMaterial(name="Carbon Compounds")
+FelsicMagma = RawMaterial(name="Felsic Magma")
+IonicSolutions = RawMaterial(name="Ionic Solutions")
+NobleGas = RawMaterial(name="Noble Gas")
+NonCSCrystals = RawMaterial(name="Non-CS Crystals")
+ReactiveGas = RawMaterial(name="Reactive Gas")
+
+# (T1)  Processed Materials
+Bacteria = ProcessedMaterials(
+    requirements={MicroOrganisms: 3000}, name="Bacteria"
+)
+Biofuels = ProcessedMaterials(
+    requirements={CarbonCompounds: 3000}, name="Biofuels"
+)
+Biomass = ProcessedMaterials(
+    requirements={PlankticColonies: 3000}, name="Biomass"
+)
+ChiralStructures = ProcessedMaterials(
+    requirements={NonCSCrystals: 3000}, name="Chiral Structures"
+)
+Electrolytes = ProcessedMaterials(
+    requirements={IonicSolutions: 3000}, name="Electrolytes"
+)
+IndustrialFibers = ProcessedMaterials(
+    requirements={Autotrophs: 3000}, name="Industrial Fibers"
+)
+OxidizingCompound = ProcessedMaterials(
+    requirements={ReactiveGas: 3000}, name="Oxidizing Compound"
+)
+Oxygen = ProcessedMaterials(
+    requirements={NobleGas: 3000}, name="Oxygen"
+)
+Plasmoids = ProcessedMaterials(
+    requirements={SuspendedPlasma: 3000}, name="Plasmoids"
+)
+PreciousMetals = ProcessedMaterials(
+    requirements={NobleMetals: 3000}, name="Precious Metals"
+)
+Proteins = ProcessedMaterials(
+    requirements={ComplexOrganisms: 3000}, name="Proteins"
+)
+ReactiveMetals = ProcessedMaterials(
+    requirements={BaseMetals: 3000}, name="Reactive Metals"
+)
+Silicon = ProcessedMaterials(
+    requirements={FelsicMagma: 3000}, name="Silicon"
+)
+ToxicMetals = ProcessedMaterials(
+    requirements={HeavyMetals: 3000}, name="Toxic Metals"
+)
+Water = ProcessedMaterials(
+    requirements={AqueousLiquids: 3000}, name="Water"
+)
+
+# (T2)  Refined Commodities
+Biocells = RefinedCommodities(
+    requirements={Biofuels: 20, PreciousMetals: 20}, name="Biocells"
+)
+ConstructionBlocks = RefinedCommodities(
+    requirements={ReactiveMetals: 20, ToxicMetals: 20},
+    name="Construction Blocks"
+)
+ConsumerElectronics = RefinedCommodities(
+    requirements={ToxicMetals: 20, ChiralStructures: 20},
+    name="Consumer Electronics"
+)
+Coolant = RefinedCommodities(
+    requirements={Electrolytes: 20, Water: 20}, name="Coolant"
+)
+EnrichedUranium = RefinedCommodities(
+    requirements={PreciousMetals: 20, ToxicMetals: 20},
+    name="Enriched Uranium"
+)
+Fertilizer = RefinedCommodities(
+    requirements={Bacteria: 20, Proteins: 20}, name="Fertilizer"
+)
+GeneticallyEnhancedLivestock = RefinedCommodities(
+    requirements={Proteins: 20, Biomass: 20},
+    name="Genetically Enhanced Livestock"
+)
+Livestock = RefinedCommodities(
+    requirements={Proteins: 20, Biofuels: 20}, name="Livestock"
+)
+MechanicalParts = RefinedCommodities(
+    requirements={ReactiveMetals: 20, PreciousMetals: 20},
+    name="Mechanical Parts"
+)
+MicrofiberShielding = RefinedCommodities(
+    requirements={IndustrialFibers: 20, Silicon: 20},
+    name="Microfiber Shielding"
+)
+MiniatureElectronics = RefinedCommodities(
+    requirements={ChiralStructures: 20, Silicon: 20},
+    name="Miniature Electronics"
+)
+Nanites = RefinedCommodities(
+    requirements={Bacteria: 20, ReactiveMetals: 20}, name="Nanites"
+)
+Oxides = RefinedCommodities(
+    requirements={OxidizingCompound: 20, Oxygen: 20}, name="Oxides"
+)
+Polyaramids = RefinedCommodities(
+    requirements={OxidizingCompound: 20, IndustrialFibers: 20},
+    name="Polyaramids"
+)
+Polytextiles = RefinedCommodities(
+    requirements={Biofuels: 20, IndustrialFibers: 20},
+    name="Polytextiles"
+)
+RocketFuel = RefinedCommodities(
+    requirements={Plasmoids: 20, Electrolytes: 20},
+    name="Rocket Fuel"
+)
+SilicateGlass = RefinedCommodities(
+    requirements={OxidizingCompound: 20, Silicon: 20},
+    name="Silicate Glass"
+)
+Superconductors = RefinedCommodities(
+    requirements={Plasmoids: 20, Water: 20},
+    name="Superconductors"
+)
+SupertensilePlastics = RefinedCommodities(
+    requirements={Oxygen: 20, Biomass: 20},
+    name="Supertensile Plastics"
+)
+SyntheticOil = RefinedCommodities(
+    requirements={Electrolytes: 20, Oxygen: 20},
+    name="Synthetic Oil"
+)
+TestCultures = RefinedCommodities(
+    requirements={Bacteria: 20, Water: 20},
+    name="Test Cultures"
+)
+Transmitter = RefinedCommodities(
+    requirements={Plasmoids: 20, ChiralStructures: 20},
+    name="Transmitter"
+)
+ViralAgent = RefinedCommodities(
+    requirements={Bacteria: 20, Biomass: 20},
+    name="Viral Agent"
+)
+WaterCooledCPU = RefinedCommodities(
+    requirements={ReactiveMetals: 20, Water: 20},
+    name="Water Cooled CPU"
+)
+
+# (T3)  Specialized Commodities
+BiotechResearchReports = SpecializedCommodities(
+    requirements={Nanites: 10, Livestock: 10, ConstructionBlocks: 10},
+    name="Biotech Research Reports"
+)
+CameraDrones = SpecializedCommodities(
+    requirements={SilicateGlass: 10, RocketFuel: 10},
+    name="Camera Drones"
+)
+Condensates = SpecializedCommodities(
+    requirements={Oxides: 10, Coolant: 10},
+    name="Condensates"
+)
+CryoprotectantSolution = SpecializedCommodities(
+    requirements={TestCultures: 10, SyntheticOil: 10, Fertilizer: 10},
+    name="Cryoprotectant Solution"
+)
+DataChips = SpecializedCommodities(
+    requirements={SupertensilePlastics: 10, MicrofiberShielding: 10},
+    name="Data Chips"
+)
+GelMatrixBiopaste = SpecializedCommodities(
+    requirements={Oxides: 10, Biocells: 10, Superconductors: 10},
+    name="Gel Matrix Biopaste"
+)
+GuidanceSystems = SpecializedCommodities(
+    requirements={WaterCooledCPU: 10, Transmitter: 10},
+    name="Guidance Systems"
+)
+HazmatDetectionSystems = SpecializedCommodities(
+    requirements={Polytextiles: 10, ViralAgent: 10, Transmitter: 10},
+    name="Hazmat Detection Systems"
+)
+HermeticMembranes = SpecializedCommodities(
+    requirements={Polyaramids: 10, GeneticallyEnhancedLivestock: 10},
+    name="Hermetic Membranes"
+)
+HighTechTransmitters = SpecializedCommodities(
+    requirements={Polyaramids: 10, Transmitter: 10},
+    name="High Tech Transmitters"
+)
+IndustrialExplosives = SpecializedCommodities(
+    requirements={Fertilizer: 10, Polytextiles: 10},
+    name="Industrial Explosives"
+)
+Neocoms = SpecializedCommodities(
+    requirements={Biocells: 10, SilicateGlass: 10}, name="Neocoms"
+)
+NuclearReactors = SpecializedCommodities(
+    requirements={EnrichedUranium: 10, MicrofiberShielding: 10},
+    name="Nuclear Reactors"
+)
+PlanetryVehicles = SpecializedCommodities(
+    requirements={
+        SupertensilePlastics: 10, MechanicalParts: 10, MiniatureElectronics: 10
+    },
+    name="Planetry Vehicles"
+)
+Robotics = SpecializedCommodities(
+    requirements={MechanicalParts: 10, ConsumerElectronics: 10},
+    name="Robotics"
+)
+SmartfabUnits = SpecializedCommodities(
+    requirements={ConstructionBlocks: 10, MiniatureElectronics: 10},
+    name="Smartfab Units"
+)
+Supercomputers = SpecializedCommodities(
+    requirements={WaterCooledCPU: 10, Coolant: 10, ConsumerElectronics: 10},
+    name="Supercomputers"
+)
+SyntheticSynapses = SpecializedCommodities(
+    requirements={SupertensilePlastics: 10, TestCultures: 10},
+    name="Synthetic Synapses"
+)
+TranscranialMicorcontrollers = SpecializedCommodities(
+    requirements={Biocells: 10, Nanites: 10},
+    name="Transcranial Micorcontrollers"
+)
+UkomiSuperconductors = SpecializedCommodities(
+    requirements={SyntheticOil: 10, Superconductors: 10},
+    name="Ukomi Superconductors"
+)
+Vaccines = SpecializedCommodities(
+    requirements={Livestock: 10, ViralAgent: 10},
+    name="Vaccines"
+)
+
+# (T4) Advanced Commodities
+BroadcastNode = AdvancedCommodities(
+    requirements={Neocoms: 6, DataChips: 6, HighTechTransmitters: 6},
+    name="Broadcast Node"
+)
+IntegrityResponseDrones = AdvancedCommodities(
+    requirements={
+        GelMatrixBiopaste: 6, HazmatDetectionSystems: 6, PlanetryVehicles: 6
+    },
+    name="Integrity Response Drones"
+)
+NanoFactory = AdvancedCommodities(
+    requirements={
+        IndustrialExplosives: 6, ReactiveMetals: 40, UkomiSuperconductors: 6
+    },
+    name="Nano Factory"
+)
+OrganicMortarApplicators = AdvancedCommodities(
+    requirements={Condensates: 6, Bacteria: 40, Robotics: 6},
+    name="Organic Mortar Applicators"
+)
+RecursiveComputingModule = AdvancedCommodities(
+    requirements={
+        SyntheticSynapses: 6,
+        GuidanceSystems: 6,
+        TranscranialMicorcontrollers: 6
+    },
+    name="Recursive Computing Module"
+)
+SelfHarmonizingPowerCore = AdvancedCommodities(
+    requirements={CameraDrones: 6, NuclearReactors: 6, HermeticMembranes: 6},
+    name="Self Harmonizing Power Core"
+)
+SterileConduits = AdvancedCommodities(
+    requirements={SmartfabUnits: 6, Water: 40, Vaccines: 6},
+    name="Sterile Conduits"
+)
+WetwareMainframe = AdvancedCommodities(
+    requirements={
+        Supercomputers: 6, BiotechResearchReports: 6, CryoprotectantSolution: 6
+    },
+    name="Wetware Mainframe"
+)
+
+# Ice
+HeavyWater = MarketItem(name="Heavy Water")
+LiquidOzone = MarketItem(name="Liquid Ozone")
+NitrogenIsotopes = MarketItem(name="Nitrogen Isotopes")
+
+
+# Items
+CaldariFuelBlocks = Manufacturable(
+    requirements={
+        EnrichedUranium: 4,
+        Oxygen: 20,
+        MechanicalParts: 4,
+        Coolant: 9,
+        Robotics: 1,
+        HeavyWater: 151,
+        LiquidOzone: 151,
+        NitrogenIsotopes: 400
+    },
+    produces=40,
+    processing_time=15 * 60,
+    name="Caldari Fuel Block"
+)
